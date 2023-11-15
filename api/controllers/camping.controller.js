c0447954407c7e212726e62167295d09ec84663f
fareh <fareh.rv@gmail.com>
2023-11-15 10:20:51 +0100

    camping controller y routes, create comentario model

---
+++ api/controllers/camping.controller.js
@@ -0,0 +1,60 @@
+const Camping = require("../models/camping.model");
+
+const getAllCampings = async (req, res, next) => {
+  try {
+    const campings = await Camping.find();
+    return res.status(200).json(campings);
+  } catch (error) {
+    return res.status(404).json("Camping no creado", error);
+  }
+};
+
+const getCampingByID = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const camping = await Camping.findById(id);
+    return res.status(200).json(camping);
+  } catch (error) {
+    return res.status(404).json("Camping no encontrado", error);
+  }
+};
+
+const createCamping = async (req, res, next) => {
+  try {
+    const newCamping = new Camping(req.body);
+    await newCamping.save();
+    return res.status(201).json(newCamping);
+  } catch (error) {
+    return res.status(500).json("camping no creado", error);
+  }
+};
+
+const deleteCamping = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Camping.findByIdAndDelete(id);
+    return res.status(200).json("Camping  borrado");
+  } catch (error) {
+    return res.status(500).json("error al borrar camping", error);
+  }
+};
+
+const updateCamping = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const newCamping = new Camping(req.body);
+    newCamping._id = id;
+    await Camping.findByIdAndUpdate(id, newCamping);
+    return res.status(200).json("camping modificado");
+  } catch (error) {
+    return res.status(500).json("Error  al modificar", error);
+  }
+};
+
+module.exports = {
+  getAllCampings,
+  getCampingByID,
+  createCamping,
+  deleteCamping,
+  updateCamping,
+};
---
+++ api/routes/camping.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  getAllCampings,
+  getCampingByID,
+  createCamping,
+  deleteCamping,
+  updateCamping,
+} = require("../controllers/camping.controller");
+
+const CampingRouter = express.Router();
+
+CampingRouter.get("/", getAllCampings);
+CampingRouter.get("/:id", getCampingByID);
+CampingRouter.post("/", createCamping);
+CampingRouter.delete("/:id", deleteCamping);
+CampingRouter.patch("/:id", updateCamping);
