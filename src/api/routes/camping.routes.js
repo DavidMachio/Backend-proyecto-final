0c3696ac4ae2f01f0237de43a93330befaa97118
fareh <fareh.rv@gmail.com>
2023-11-23 01:00:55 +0100

    pagination

--- src/api/routes/camping.routes.js
+++ src/api/routes/camping.routes.js
@@ -7,12 +7,14 @@ const {
   deleteCamping,
   updateCamping,
   getCampingByProv,
+  getAllByPage,
 } = require("../controllers/camping.controller");
 const { isAuth, isAdmin } = require("../../middlewares/auth.middleware");
 
 const CampingRouter = express.Router();
 
 CampingRouter.get("/", getAllCampings);
+CampingRouter.get("/paginado", getAllByPage);
 CampingRouter.get("/name/:name", getCampingByName);
 CampingRouter.get("/id/:id", getCampingByID);
 CampingRouter.post("/", createCamping);
