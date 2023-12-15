ef6002de849c21f9bebbd6a726997ae1bda40d54
David <D_machio@hotmail.com>
2023-12-15 10:06:45 +0100

    despliegue

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ configCloudinary();
 
 server.use(
   cors({
-    origin: ["http://localhost:5173", "https://campcesible.vercel.app/"],
+    origin: ["http://localhost:5173", "https://campcesible.vercel.app"],
   })
 );
 server.use(express.json());
